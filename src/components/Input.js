5bdc9b088a097ffcc32a99b6394e4ffe8f1ded4c
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-03-06 17:13:11 +0530

    small changes added

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
 
 function Input() {
 
   const [inputText, setInputText] = useState("");
   const [todo,setTodo] = useState([]);
+  const [edit , setEdit] = useState(false);
 
 
   const onAddTodo = () => {
@@ -13,6 +15,13 @@ function Input() {
     }
   }
 
+  const DeleteTodo = (uuidv4) => {
+    let deleteTodo = [...todo];
+    deleteTodo.splice(uuidv4,1);
+    setTodo(deleteTodo);
+  }
+
+
   return (
     <div>
       <input 
@@ -20,18 +29,18 @@ function Input() {
       placeholder='Write your todo' 
       value={inputText}
       onChange={e => setInputText(e.target.value)}
+      
       />
 
-      <button onClick={onAddTodo}> 
-       Add
-      </button>
+      <button  key={uuidv4} onClick={onAddTodo}> Add </button>
 
       <div>
        
-        {todo.map((t)=>(
+        {todo.map((t,id)=>(
           <>
-            <textarea style={{resize : "none",height : "100px",width : "200px", backgroundColor: "#4d4d4d"}} key={t}>{t}</textarea>
-            <i className="fa-solid fa-trash"></i>
+            <h1 style={{resize : "none",height : "100px",width : "200px", backgroundColor: "#4d4d4d"}} key={t} contentEditable={edit}>{t}</h1>
+            <i className="fa-solid fa-trash" key={id} onClick={DeleteTodo}></i>
+            <i className="fa-solid fa-pen" onClick={()=> setEdit(true)}></i>
           </>
         ))}
       </div>
