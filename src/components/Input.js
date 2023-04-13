16c4e6a2039f7af314fb8392db118d1a8579336e
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-04-13 19:22:04 +0530

    some random changes

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import {v4 as uuidv4} from 'uuid';
+import {v4} from 'uuid';
 
 function Input() {
 
   const [inputText, setInputText] = useState("");
   const [todo,setTodo] = useState([]);
-  const [edit , setEdit] = useState(false);
+  const [edit , setEdit] = useState(true);
 
 
   const onAddTodo = () => {
@@ -15,32 +15,37 @@ function Input() {
     }
   }
 
-  const DeleteTodo = (uuidv4) => {
+  const DeleteTodo = (v4) => {
     let deleteTodo = [...todo];
-    deleteTodo.splice(uuidv4,1);
+    deleteTodo.splice(v4,1);
     setTodo(deleteTodo);
   }
-
 
   return (
     <div>
       <input 
-      type="text" 
-      placeholder='Write your todo' 
-      value={inputText}
-      onChange={e => setInputText(e.target.value)}
-      
+        type="text" 
+        placeholder='Write your todo' 
+        value={inputText}
+        onChange={e => setInputText(e.target.value)}
+
       />
 
-      <button  key={uuidv4} onClick={onAddTodo}> Add </button>
+      <button onClick={onAddTodo}> Add </button>
 
       <div>
        
-        {todo.map((t,id)=>(
+        {todo.map((t)=>(
           <>
-            <h1 style={{resize : "none",height : "100px",width : "200px", backgroundColor: "#4d4d4d"}} key={t} contentEditable={edit}>{t}</h1>
-            <i className="fa-solid fa-trash" key={id} onClick={DeleteTodo}></i>
-            <i className="fa-solid fa-pen" onClick={()=> setEdit(true)}></i>
+            <input 
+              type="text"
+              disabled={edit}
+              value={t}
+              onChange={(e)=> t = e.target.value}
+              key={v4()}
+            />
+            <i className="fa-solid fa-trash" key={v4()} onClick={DeleteTodo}></i>
+            <i className="fa-solid fa-edit" key={v4()} onClick={()=> setEdit(!edit)}></i>
           </>
         ))}
       </div>
